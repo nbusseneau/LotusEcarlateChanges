f16a3dcff6153d18dc21a289b554678e92928269
Nicolas Busseneau <nicolas@busseneau.fr>
2024-06-06 05:09:22 +0200

    rebalance some vanilla pieces recipes

--- src/Changes/Manual/VanillaMisc.cs
+++ src/Changes/Manual/VanillaMisc.cs
@@ -7,6 +7,9 @@ public class VanillaMisc : ManualChangesBase
 {
   protected override void ApplyInternalDeferred()
   {
+    var workbench = ZNetScene.instance.GetPrefab(Jotunn.Configs.CraftingStations.Workbench).GetComponent<CraftingStation>();
+    var forge = ZNetScene.instance.GetPrefab(Jotunn.Configs.CraftingStations.Forge).GetComponent<CraftingStation>();
+
     // Relocate adornements from Misc to Furniture for consistency
     string[] adornements = [
       "darkwood_raven",
@@ -14,6 +17,22 @@ public class VanillaMisc : ManualChangesBase
       "wood_dragon1",
     ];
     foreach (var piece in this.PieceManager[adornements]) piece.Category = Piece.PieceCategory.Furniture;
+
+    // Rebalance some additional pieces
+    this.PieceManager["piece_hexagonal_door"].CraftingStation = forge;
+    this.PieceManager["piece_hexagonal_door"].Category = Piece.PieceCategory.BuildingStonecutter;
+
+    this.PieceManager["darkwood_gate"].CraftingStation = workbench;
+
+    var dvergrSharpStakes = this.PieceManager["piece_dvergr_sharpstakes"];
+    dvergrSharpStakes.CraftingStation = workbench;
+    dvergrSharpStakes.Resources.Clear();
+    dvergrSharpStakes.Resources.Add("YggdrasilWood", 6);
+
+    var dvergrStakewall = this.PieceManager["piece_dvergr_stake_wall"];
+    dvergrStakewall.CraftingStation = workbench;
+    dvergrStakewall.Resources.Clear();
+    dvergrStakewall.Resources.Add("YggdrasilWood", 4);
 
     // Skills
     this.ItemManager["THSwordKrom"].Weapon.SkillType = CustomSkills.TwoHandedSwords;
