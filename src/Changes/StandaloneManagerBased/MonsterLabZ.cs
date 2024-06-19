f34e195de3e9d736302961bc997c8e0bb1b39a17
Nicolas Busseneau <nicolas@busseneau.fr>
2024-06-19 22:47:51 +0200

    rebalance MonsterLabZ dwarf goblins

--- src/Changes/StandaloneManagerBased/MonsterLabZ.cs
+++ src/Changes/StandaloneManagerBased/MonsterLabZ.cs
@@ -1,6 +1,7 @@
 extern alias MonsterLabZ;
 
 using LotusEcarlateChanges.Model.Changes;
+using MonsterLabZ::ItemManager;
 using MonsterLabZ::CreatureManager;
 
 namespace LotusEcarlateChanges.Changes.StandaloneManagerBased;
@@ -9,8 +10,18 @@ public class MonsterLabZ : StandaloneManagerBasedChangesBase
 {
   protected override void ApplyInternal()
   {
-    var creatureManager = this.RegisterCreatureManager(Creature.registeredCreatures, PrefabManager.prefabs);
+    var itemManager = this.RegisterItemManager(Item.registeredItems, MonsterLabZ::ItemManager.PrefabManager.prefabs, MonsterLabZ::ItemManager.PrefabManager.ZnetOnlyPrefabs);
+    var creatureManager = this.RegisterCreatureManager(Creature.registeredCreatures, MonsterLabZ::CreatureManager.PrefabManager.prefabs);
     var locationManager = this.RegisterLocationManager(MonsterLabZ::LocationManager.Location.registeredLocations);
+
+    // Damage
+    var dwarfGoblinShamanFireballDamages = itemManager["DwarfGoblinShaman_attack_fireball"].Wrapper.Weapon.Damages;
+    dwarfGoblinShamanFireballDamages.m_fire = 50f;
+    itemManager["DwarfGoblinShaman_attack_fireball"].Wrapper.Weapon.Damages = dwarfGoblinShamanFireballDamages;
+
+    var dwarfGoblinShamanBoatFireballDamages = itemManager["DwarfGoblinShamanBoat_attack_fireball"].Wrapper.Weapon.Damages;
+    dwarfGoblinShamanBoatFireballDamages.m_fire = 50f;
+    itemManager["DwarfGoblinShamanBoat_attack_fireball"].Wrapper.Weapon.Damages = dwarfGoblinShamanBoatFireballDamages;
 
     // Drops
     creatureManager["BrownSpider"].Drops.drops.Remove("Ooze");
@@ -24,12 +35,21 @@ public class MonsterLabZ : StandaloneManagerBasedChangesBase
     creatureManager["Green_Butterfly"].Drops.drops.Remove("Ooze");
     creatureManager["Green_Butterfly"].Drops.drops["Amber"] = new();
 
+    creatureManager["DwarfGoblin"].Drops.drops["Coins"].Amount = new(0, 15);
+    creatureManager["DwarfGoblin"].Drops.drops["Amber"] = new() { Amount = new(0, 3) };
+    creatureManager["DwarfGoblin"].Drops.drops["Ruby"] = new() { DropChance = 50f };
+
+    creatureManager["DwarfGoblinShaman"].Drops.drops["Coins"].Amount = new(0, 25);
+    creatureManager["DwarfGoblinShaman"].Drops.drops["Amber"] = new() { Amount = new(0, 3) };
+    creatureManager["DwarfGoblinShaman"].Drops.drops["Ruby"] = new() { DropChance = 50f };
+
     // Spawns
     creatureManager.Remove([
       "ML_AshHatchling",
       "ML_AshHuldra",
       "ML_FrostHatchling",
       "FireGolem",
+      "DwarfGoblinLoot",
     ]);
 
     creatureManager["TreeSpider"].SpawnChance = 5f;
@@ -44,6 +64,10 @@ public class MonsterLabZ : StandaloneManagerBasedChangesBase
     creatureManager["ML_GoblinShip"].Biome = Heightmap.Biome.Ocean;
     creatureManager["ML_GoblinShip"].RequiredWeather = Weather.None;
     creatureManager["ML_GoblinShip"].SpecificSpawnArea = MonsterLabZ::CreatureManager.SpawnArea.Everywhere;
+
+    creatureManager["DwarfGoblin"].GroupSize = new(2, 4);
+    creatureManager["DwarfGoblin"].Maximum = 4;
+    creatureManager["DwarfGoblinShaman"].CanHaveStars = false;
 
     // Locations
     locationManager.Remove([
